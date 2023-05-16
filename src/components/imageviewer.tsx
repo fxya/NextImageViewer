e9f549189fac695413236bbd543b941e2e786c40
Faisal Ahsan <f.ahsan.07@gmail.com>
2023-05-16 11:44:45 +0100

    Add example of how metadata such as alt text could be rendered

--- src/components/imageviewer.tsx
+++ src/components/imageviewer.tsx
@@ -1,13 +1,19 @@
-import React, {useState} from 'react'
-import styles from "../styles/styles.module.css";
+import React, { useState } from 'react';
+import styles from '../styles/styles.module.css';
 
+type Image = {
+    src: string;
+    alt: string;
+    metadata: string;
+};
 
 type Props = {
-    images: string[]
-}
+    images: Image[];
+};
 
-const ImageViewer = ({images}: Props): JSX.Element => {
+const ImageViewer = ({ images }: Props): JSX.Element => {
     const [currentImageIndex, setCurrentImageIndex] = useState<number>(0);
+    const currentImage = images[currentImageIndex];
 
     const handlePrevClick = (): void => {
         setCurrentImageIndex((prevIndex) =>
@@ -16,24 +22,19 @@ const ImageViewer = ({images}: Props): JSX.Element => {
     };
 
     const handleNextClick = (): void => {
-        setCurrentImageIndex((prevIndex) =>
-            (prevIndex + 1) % images.length
-        );
+        setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
     };
 
     return (
         <div className={styles.image}>
-            <img src={images[currentImageIndex]} alt={'alt'}/>
+            <img src={currentImage.src} alt={currentImage.alt} />
+            <div className={styles.metadata}>{currentImage.metadata}<br/>Alt text: {currentImage.alt}</div>
             <div className={styles.buttons}>
-                <button onClick={handlePrevClick}>
-                    Previous
-                </button>
-                <button onClick={handleNextClick}>
-                    Next
-                </button>
+                <button onClick={handlePrevClick}>Previous</button>
+                <button onClick={handleNextClick}>Next</button>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default ImageViewer
+export default ImageViewer;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,32 @@
-import React from 'react'
-import ImageViewer from '../components/imageviewer'
-import styles from "../styles/styles.module.css";
+import React from 'react';
+import ImageViewer from '../components/imageviewer';
+import styles from '../styles/styles.module.css';
 
-
-const index = [
-    'https://picsum.photos/200/300',
-    'https://picsum.photos/250/350',
-    'https://picsum.photos/300/400'
-]
+const images = [
+    {
+        src: 'https://picsum.photos/200/300',
+        alt: 'Image 1',
+        metadata: 'Example metadata for Image 1',
+    },
+    {
+        src: 'https://picsum.photos/250/350',
+        alt: 'Image 2',
+        metadata: 'Example metadata for Image 2',
+    },
+    {
+        src: 'https://picsum.photos/300/400',
+        alt: 'Image 3',
+        metadata: 'Example metadata for Image 3',
+    },
+];
 
 const IndexPage = () => {
     return (
         <div className={styles.container}>
             <h1>My Image Viewer App</h1>
-            <ImageViewer images={index}/>
+            <ImageViewer images={images} />
         </div>
-    )
-}
+    );
+};
 
-export default IndexPage
+export default IndexPage;
