375edd794b64def01c94ef967ebc60bf47abbb35
Faisal Ahsan <f.ahsan.07@gmail.com>
2023-05-16 12:39:19 +0100

    Add exifr library and async function to retrieve exif data for each image displayed

--- src/components/imageviewer.tsx
+++ src/components/imageviewer.tsx
@@ -1,38 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styles from '../styles/styles.module.css';
 
-type Image = {
-    src: string;
-    alt: string;
-    metadata: string;
-};
-
 type Props = {
-    images: Image[];
+    images: string[];
+    currentImageIndex: number;
 };
 
-const ImageViewer = ({ images }: Props): JSX.Element => {
-    const [currentImageIndex, setCurrentImageIndex] = useState<number>(0);
+const ImageViewer = ({ images, currentImageIndex }: Props): JSX.Element => {
     const currentImage = images[currentImageIndex];
-
-    const handlePrevClick = (): void => {
-        setCurrentImageIndex((prevIndex) =>
-            (prevIndex - 1 + images.length) % images.length
-        );
-    };
-
-    const handleNextClick = (): void => {
-        setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-    };
 
     return (
         <div className={styles.image}>
-            <img src={currentImage.src} alt={currentImage.alt} />
-            <div className={styles.metadata}>{currentImage.metadata}<br/>Alt text: {currentImage.alt}</div>
-            <div className={styles.buttons}>
-                <button onClick={handlePrevClick}>Previous</button>
-                <button onClick={handleNextClick}>Next</button>
-            </div>
+            <img src={currentImage} alt={'Example alt text'} />
         </div>
     );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ImageViewer from '../components/imageviewer';
 import styles from '../styles/styles.module.css';
+import exifr from 'exifr';
 
-const images = [
-    {
-        src: 'https://picsum.photos/200/300',
-        alt: 'Image 1',
-        metadata: 'Example metadata for Image 1',
-    },
-    {
-        src: 'https://picsum.photos/250/350',
-        alt: 'Image 2',
-        metadata: 'Example metadata for Image 2',
-    },
-    {
-        src: 'https://picsum.photos/300/400',
-        alt: 'Image 3',
-        metadata: 'Example metadata for Image 3',
-    },
+const index = [
+    'https://picsum.photos/200/300',
+    'https://picsum.photos/250/350',
+    'https://picsum.photos/300/400'
 ];
 
 const IndexPage = () => {
+    const [currentImageIndex, setCurrentImageIndex] = useState<number>(0);
+    const [exifData, setExifData] = useState<Record<string, any> | null>(null);
+
+    useEffect(() => {
+        const loadImageExifData = async () => {
+            const currentImage = index[currentImageIndex];
+            const data = await exifr.parse(currentImage);
+            setExifData(data);
+        };
+
+        loadImageExifData();
+    }, [currentImageIndex]);
+
+    const handlePrevClick = (): void => {
+        setCurrentImageIndex((prevIndex) =>
+            (prevIndex - 1 + index.length) % index.length
+        );
+    };
+
+    const handleNextClick = (): void => {
+        setCurrentImageIndex((prevIndex) =>
+            (prevIndex + 1) % index.length
+        );
+    };
+
     return (
         <div className={styles.container}>
             <h1>My Image Viewer App</h1>
-            <ImageViewer images={images} />
+            <ImageViewer images={index} currentImageIndex={currentImageIndex} />
+
+            {exifData && (
+                <div className={styles.exifData}>
+                    <h2>EXIF Data</h2>
+                    <pre>{JSON.stringify(exifData, null, 2)}</pre>
+                </div>
+            )}
+
+            <div className={styles.buttons}>
+                <button onClick={handlePrevClick}>Previous</button>
+                <button onClick={handleNextClick}>Next</button>
+            </div>
         </div>
     );
 };
