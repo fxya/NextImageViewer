f931908513f2f72ae2d34def3d19a31f7f9d9215
Faisal Ahsan <f.ahsan.07@gmail.com>
2023-05-22 16:24:57 +0100

    Bit more attempted work on getting a file picker working

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
-import React, { useState, useCallback } from 'react';
-import { FileContent, useFilePicker } from 'use-file-picker';
+import React, { useState, useCallback, useEffect } from 'react';
+import {FileContent, FileErrors, SelectedFiles, useFilePicker} from 'use-file-picker';
 import ImageViewer from '../components/ImageViewer';
 import ButtonContainer from '../components/ButtonContainer';
 import ExifDataViewer from '../components/ExifDataViewer';
 import ExifDataLoader from '../components/ExifDataLoader';
 import styles from '../styles/styles.module.css';
 
-const index  = [
+const index = [
     'https://picsum.photos/200/300',
     'https://picsum.photos/250/350',
     'https://picsum.photos/300/400'
@@ -14,46 +14,45 @@ const index  = [
 
 const IndexPage = () => {
     const [currentImageIndex, setCurrentImageIndex] = useState<number>(0);
+    const [filesContent, setFilesContent] = useState<FileContent[]>([]);
+    const [errors, setErrors] = useState<FileErrors>();
 
-    const [openFileSelector, { loading, filesContent, errors }] = useFilePicker({
+    const handlePrevClick = useCallback(() => {
+        setCurrentImageIndex((prevIndex) => (prevIndex - 1 + index.length) % index.length);
+    }, []);
+
+    const handleNextClick = useCallback(() => {
+        setCurrentImageIndex((prevIndex) => (prevIndex + 1) % index.length);
+    }, []);
+
+    const [openFileSelector, { loading }] = useFilePicker({
         readAs: 'DataURL',
         accept: 'image/*',
-        multiple: true,
+        multiple: false,
         limitFilesConfig: { max: 1 },
-        // minFileSize: 0.1, // in megabytes
         maxFileSize: 50,
-        imageSizeRestrictions: {
-            maxHeight: 900, // in pixels
-            maxWidth: 1600,
-            minHeight: 600,
-            minWidth: 768,
+        onFilesSuccessfulySelected: (selectedFiles) => {
+            const uploadedImage = selectedFiles.filesContent
+            setFilesContent(uploadedImage);
+            const updatedIndex = [...index];    // Create a copy of the index array via the spread operator
+
+            if (uploadedImage !== undefined) {
+                index.push(uploadedImage[0].content as string); // Support a single file upload
+            }
+            console.log(index);
+            setCurrentImageIndex(updatedIndex.length);  // Set the current image index to the last image in the array
         },
+        onFilesRejected: (fileErrors: FileErrors) => {
+            setErrors(fileErrors);
+            console.log(fileErrors);
+        }
     });
-
-    const handlePrevClick = useCallback((): void => {
-        setCurrentImageIndex((prevIndex) =>
-            (prevIndex - 1 + index.length) % index.length
-        );
-    }, []);
-
-    const handleNextClick = useCallback((): void => {
-        setCurrentImageIndex((prevIndex) =>
-            (prevIndex + 1) % index.length
-        );
-    }, []);
-
-    const handleUploadImage = useCallback((filesContent: FileContent[]): void => {
-        index.push(filesContent[0].content);
-        setCurrentImageIndex((prevIndex) =>
-            (prevIndex + 1) % index.length
-        );
-    }, []);
 
     if (loading) {
         return <div>Loading...</div>;
     }
 
-    if (errors.length) {
+    if (errors) {
         return <div>Error...</div>;
     }
 
