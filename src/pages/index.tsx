4ffef71a3f6b6bcc62d63f45f6d5faa54f449567
Faisal Ahsan <f.ahsan.07@gmail.com>
2023-05-22 16:41:12 +0100

    Refactoring

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,28 @@
 import React, { useState, useCallback, useEffect } from 'react';
-import {FileContent, FileErrors, SelectedFiles, useFilePicker} from 'use-file-picker';
+import { FileContent, FileErrors, useFilePicker } from 'use-file-picker';
 import ImageViewer from '../components/ImageViewer';
 import ButtonContainer from '../components/ButtonContainer';
 import ExifDataViewer from '../components/ExifDataViewer';
 import ExifDataLoader from '../components/ExifDataLoader';
 import styles from '../styles/styles.module.css';
 
-const index = [
+const initialIndex = [
     'https://picsum.photos/200/300',
     'https://picsum.photos/250/350',
     'https://picsum.photos/300/400'
 ];
 
 const IndexPage = () => {
-    const [currentImageIndex, setCurrentImageIndex] = useState<number>(0);
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
     const [filesContent, setFilesContent] = useState<FileContent[]>([]);
-    const [errors, setErrors] = useState<FileErrors>();
+    const [errors, setErrors] = useState<FileErrors | null>(null);
 
     const handlePrevClick = useCallback(() => {
-        setCurrentImageIndex((prevIndex) => (prevIndex - 1 + index.length) % index.length);
+        setCurrentImageIndex((prevIndex) => (prevIndex - 1 + initialIndex.length) % initialIndex.length);
     }, []);
 
     const handleNextClick = useCallback(() => {
-        setCurrentImageIndex((prevIndex) => (prevIndex + 1) % index.length);
+        setCurrentImageIndex((prevIndex) => (prevIndex + 1) % initialIndex.length);
     }, []);
 
     const [openFileSelector, { loading }] = useFilePicker({
@@ -32,21 +32,22 @@ const IndexPage = () => {
         limitFilesConfig: { max: 1 },
         maxFileSize: 50,
         onFilesSuccessfulySelected: (selectedFiles) => {
-            const uploadedImage = selectedFiles.filesContent
-            setFilesContent(uploadedImage);
-            const updatedIndex = [...index];    // Create a copy of the index array via the spread operator
-
-            if (uploadedImage !== undefined) {
-                index.push(uploadedImage[0].content as string); // Support a single file upload
-            }
-            console.log(index);
-            setCurrentImageIndex(index.length - 1);  // Set the current image index to the last image in the array
+            const uploadedImage = selectedFiles.filesContent[0];
+            setFilesContent([uploadedImage]);
+            setCurrentImageIndex(initialIndex.length);
         },
         onFilesRejected: (fileErrors: FileErrors) => {
             setErrors(fileErrors);
-            console.log(fileErrors);
         }
     });
+
+    useEffect(() => {
+        if (filesContent.length > 0) {
+            const updatedIndex = [...initialIndex];
+            updatedIndex.push(filesContent[0].content as string);
+            setCurrentImageIndex(updatedIndex.length);
+        }
+    }, [filesContent]);
 
     if (loading) {
         return <div>Loading...</div>;
@@ -59,13 +60,13 @@ const IndexPage = () => {
     return (
         <div className={styles.container}>
             <h1>My Image Viewer App</h1>
-            <ImageViewer images={index} currentImageIndex={currentImageIndex} />
+            <ImageViewer images={initialIndex} currentImageIndex={currentImageIndex} />
             <ButtonContainer
                 uploadImage={openFileSelector}
                 handlePrevClick={handlePrevClick}
                 handleNextClick={handleNextClick}
             />
-            <ExifDataViewer exifData={ExifDataLoader(index, currentImageIndex)} />
+            <ExifDataViewer exifData={ExifDataLoader(initialIndex, currentImageIndex)} />
         </div>
     );
 };
